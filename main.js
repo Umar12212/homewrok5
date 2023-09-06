87b3c2fc0cf6e087a3a005052c675a28ab7eb1da
scroll <96938159+Umar12212@users.noreply.github.com>
2023-09-06 15:07:50 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let name = prompt("Введите ваше имя: ");
-let year = parseInt(prompt("Введите ваш год рождения: "));
-let nowyear = parseInt(prompt("Введите текущий год: "));
+let year = prompt("Введите ваш год рождения: ");
+let nowyear = prompt("Введите текущий год: ");
 
 let age = nowyear - year;
 
